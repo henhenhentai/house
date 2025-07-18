cb5eb7ff78920fb64450fde3cf76bd75d83be4b4
1 <1802951884@qq.com>
2025-07-18 11:34:36 +0800

    1

---
+++ config.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+# 创建flask-sqlalchemy的实例对象
+db = SQLAlchemy()
+
+class Config:
+    # 调试模式
+    DEBUG = False
+    # 指定数据库的链接地址
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:wxhyiyiyi.@127.0.0.1/house'
+    # 关闭警告信息
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
