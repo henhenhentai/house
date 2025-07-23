cc0dd582f8cd9af39a340076f59e69e099514c7a
1 <1802951884@qq.com>
2025-07-23 15:58:52 +0800

    1

--- page/list.py
+++ page/list.py
@@ -1,13 +1,32 @@
 from flask import Flask, Blueprint, render_template
+from models import House,Recommend;
+import math
 
-# 创建蓝图，蓝图名称为index_page
 list_page = Blueprint('list_page', __name__)
 
-@list_page.route('/list')
-def index():
-    return render_template('list.html')
+@list_page.route('/list/pattern/<int:page>')
+def index(page):
+    # 获取房源总数
+    house_total = House.query.count()
+    # 计算页码数，向上取整
+    total_num  = math.ceil(house_total / 10)
+
+    # 查询每一页的数据  paginate两个参数，一个是当前页，一个每页显示的数量
+    result = House.query.order_by(House.publish_time.desc()).paginate(page=page,per_page=10)
+
+    return render_template('list.html', page_num = result.page, house_list=result.items, total_num=total_num)
 
 
+@list_page.route('/list/hot_house/<int:page>')
+def hot(page):
+    # 获取房源总数
+    house_total = House.query.count()
+    # 计算页码数，向上取整
+    total_num = math.ceil(house_total / 10)
 
+    # 查询每一页的数据  paginate两个参数，一个是当前页，一个每页显示的数量
+    result = House.query.order_by(House.page_views.desc()).paginate(page=page, per_page=10)
 
+   # result = House.query.order_by(House.publish_time.desc()).paginate(page=page, per_page=10)
 
+    return render_template('list.html', page_num=result.page, house_list=result.items, total_num=total_num)
--- page/query.py
+++ page/query.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, render_template, request, jsonify
 from models import House;
-
+import math
 query_page = Blueprint('query_page', __name__)
 
 
@@ -39,3 +39,15 @@ def deal_direction(word):
 # 添加过滤器
 query_page.add_app_template_filter(deal_title_over, 'dealover')
 query_page.add_app_template_filter(deal_direction, 'dealdirection')
+
+def index(page):
+    # 获取房源总数
+    house_total = House.query.count()
+    # 计算页码数，向上取整
+    total_num  = math.ceil(house_total / 10)
+
+    # 查询每一页的数据  paginate两个参数，一个是当前页，一个每页显示的数量
+
+    result = House.query.order_by(House.publish_time.desc()).paginate(page=page,per_page=10)
+
+    return render_template('list.html', page_num = result.page, house_list=result.items, total_num=total_num)
