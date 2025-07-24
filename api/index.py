be74a2fb939978c93533067d9931e93e0cea48c4
henhenhentai <1802951884@qq.com>
2025-07-24 08:55:09 +0800

    Delete api/index.py

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-from models import House # 确保您已经从 models 文件导入了 House 模型
-from config import db   # 确保您已经导入了 db 实例
-
-house_api = Blueprint('house_api', __name__)
-
-# @house_api.route('/search', methods=['POST'])
-# def search():
-
